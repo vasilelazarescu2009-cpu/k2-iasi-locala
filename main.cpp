d87c0103a721eab5fedd802bfb700414f7a8c007
unknown <vasilelazarescu2009@gmail.com>
2026-02-14 19:15:53 +0200

    version 1

---
+++ main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*int peak(int n)
+{
+    bool creste = false;
+    int t = 2;
+    int Max = 2;
+    while(n > 0)
+    {
+        //cout << t << " ";
+        if(creste)
+        {
+            t++;
+            if(Max < t)
+            {
+                Max = t;
+                creste = false;
+            }
+        }
+        else
+        {
+            t--;
+            if(t <= 1)
+                creste = true;
+        }
+        n--;
+
+
+
+    }
+    return Max;
+}
+
+int last(int n){
+       bool creste = false;
+    int t = 2;
+    int Max = 2;
+    while(n > 1)
+    {
+
+        if(creste)
+        {
+            t++;
+            if(Max < t)
+            {
+                Max = t;
+                creste = false;
+            }
+        }
+        else
+        {
+            t--;
+            if(t <= 1)
+                creste = true;
+        }
+        n--;
+
+         //cout << t << " ";
+
+    }
+    return t;
+}
+*/
+int main()
+{
+    int n, c;
+    ifstream in("k2.in");
+    ofstream out("k2.out");
+    in >> c >> n;
+    bool creste = false;
+    int t = 2;
+    int Max = 2;
+    while(n > 1)
+    {
+
+        if(creste)
+        {
+            t++;
+            if(Max < t)
+            {
+                Max = t;
+                creste = false;
+            }
+        }
+        else
+        {
+            t--;
+            if(t <= 1)
+                creste = true;
+        }
+        n--;
+
+         cout << t << " ";
+
+    }
+    if(c==1)
+        out << Max;
+    else if(c==2)
+        out << t;
+    return 0;
+}
